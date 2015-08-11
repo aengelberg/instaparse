3a9251182554e5aaade926b4b911057103e60c12
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2015-08-11 11:17:54 -0700

    Change project.clj so building / testing still works on Clojure 1.7

--- project.clj
+++ project.clj
@@ -3,37 +3,17 @@
   :url "https://github.com/lbradstreet/instaparse-cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
-                 [org.clojure/clojurescript "0.0-3308"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.28"]]
   :profiles {:dev {:dependencies 
                    [[org.clojure/tools.trace "0.7.5"]
                     [criterium "0.3.1"]
                     [rhizome "0.1.8"]]
-                   :plugins [[lein-figwheel "0.3.3"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-RC2"]]}}
-  :aliases {"test-all" ["with-profile" "+1.5:+1.6:+1.7" "test"]
-            "cleantestcljs" ["do" "clean," "cljx" "once," "cljsbuild" "test" "unit-tests"]}
-  :test-paths ["target/generated/src/clj" "target/generated/test/clj"]
-  :source-paths ["src/cljs" "src/clj"]
-  :clj {:source-paths  ["src/clj",  "target/generated/src/clj"]
-        :test-paths  ["test/clj",  "target/generated/test/clj"]}
-  :cljx {:builds [{:source-paths  ["src/cljx"]
-                   :output-path "target/generated/src/clj"
-                   :rules :clj}
-                  {:source-paths ["src/cljx"]
-                   :output-path "target/generated/src/cljs"
-                   :rules :cljs}
-                  {:source-paths  ["test/cljx"]
-                   :output-path "target/generated/test/clj"
-                   :rules :clj}
-                  {:source-paths  ["test/cljx"]
-                   :output-path  "target/generated/test/cljs"
-                   :rules :cljs}]}
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [com.keminglabs/cljx "0.6.0" :exclusions [org.clojure/clojure]]]
-  :prep-tasks [["cljx" "once"]]
+                   :plugins [[lein-figwheel "0.3.3"]]}}
+  :aliases {"cleantestcljs" ["do" "clean," "cljsbuild" "test" "unit-tests"]}
+  :test-paths ["test/"]
+  :source-paths ["src/cljs" "src/clj" "src/cljc"]
+  :plugins [[lein-cljsbuild "1.0.6"]]
   ;:hooks [leiningen.cljsbuild]
   :target-path "target"
   :scm {:name "git"
@@ -46,8 +26,7 @@
                        {:id "test"
                         :source-paths ["src/cljs" 
                                        "runner/cljs"
-                                       "target/generated/test/clj"
-                                       "target/generated/test/cljs"]
+                                       "test/"]
                         :compiler {:output-to "target/js/advanced-test.js"
                                    :optimizations :advanced
                                    :target :nodejs
